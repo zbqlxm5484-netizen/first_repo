39bc0056411f0a2487272ca631118ecdbbc0adc9
zbqlxm5484-netizen <zbqlxm5484@gmail.com>
2026-02-24 15:37:53 +0900

    [Silver I] Title: 나이트의 이동, Time: 476 ms, Memory: 146376 KB - DashHub

---
+++ BOJ/Silver/7562. 나이트의 이동/Main.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+for tc in range(1,int(input())+1) :
+    n = int(input())
+    a,b = map(int,input().split())
+    c,d = map(int,input().split())
+    visited = [[0]*n for _ in range(n)]
+    visited[a][b] = 1
+    dq = deque()
+    dq.append((a,b))
+
+    while dq :
+        i,j = dq.popleft()
+        if i == c and j == d :
+            print(visited[i][j]-1)
+        for ni,nj in [[1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]] :
+            di , dj = i + ni, j +nj
+            if 0 <= di < n and 0 <= dj < n : 
+                if not visited[di][dj] :
+                    visited[di][dj] = visited[i][j] + 1
+                    dq.append((di,dj))
